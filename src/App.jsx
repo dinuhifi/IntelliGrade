bf5de73da4f823190118172ec78a3371f9977f27
Dinesh <dinesh23110077@snuchennai.edu.in>
2025-03-09 20:38:25 +0530

    Added AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import LandingPage from './pages/LandingPage'
-import TeacherLogin from './pages/TeacherLogin'
-import TeacherSignup from './pages/TeacherSignup'
-import StudentLogin from './pages/StudentLogin'
-import StudentSignup from './pages/StudentSignup'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
+import AppRoutes from './routes/AppRoutes'
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/teacher/login" element={<TeacherLogin />} />
-          <Route path="/teacher/signup" element={<TeacherSignup />} />
-          <Route path="/student/login" element={<StudentLogin />} />
-          <Route path="/student/signup" element={<StudentSignup />} />
-        </Routes>
-      </Router>
+      <AppRoutes/>
     </>
   )
 }
