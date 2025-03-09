f901bc2ea42dc84516dcf40a8de57f2fe1ee7bf2
Dinesh <dinesh23110077@snuchennai.edu.in>
2025-03-09 17:30:31 +0530

    Added Navbar for landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-10xl font-bold underline italic">
-      Hello world!
-    </h1>
+      <main className='overflow-x-hidden antialiased textneutral-800'>
+        <Navbar />
+      </main>
     </>
   )
 }
